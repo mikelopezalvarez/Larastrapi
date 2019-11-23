9aa117c8bdc1f626ce53386b04fe1c9504c0ddc3
Ivan Mercado <ivanmercado9@gmail.com>
2019-11-23 17:05:31 -0400

    Fix Create App form styles

--- resources/js/admin/DataManagement/components/AppCreator.vue
+++ resources/js/admin/DataManagement/components/AppCreator.vue
@@ -2,67 +2,35 @@
 
 
   <div class="pa-5">
-
-    <!--
-    <v-alert icon="mdi-shield-lock-outline" prominent text type="info">
-      <strong>Donec quam felis, ultricies.</strong> Sed in libero ut nibh placerat accumsan.. Curabitur blandit mollis lacus. Curabitur blandit mollis lacus.
-    </v-alert>-->
-    <v-row class="">
-      <v-col md="12">
-        <v-card class="pa-3" outlined tile>
+    <v-row class="" justify="center">
+      <v-col sm="12" md="8">
+        <v-card class="pa-3" >
           
           <v-card-text>
-            <!--<div>Word of the Day</div>-->
-            <p class="title text--primary ">
-              CREATE NEW APPLICATION
-              
-            </p>
+            <p class="title text--primary "> CREATE NEW APPLICATION </p>
 
-            <v-text-field
-                v-model="app.name"
-                :counter="10"
-                label="Name"
-                required
-            ></v-text-field>
-
-            <v-textarea
-                v-model="app.descrip"
-                name="input-7-1"
-                label="Description"
-                value="The Woodman set to work at once, and so sharp was his axe that the tree was soon chopped nearly through."
-                ></v-textarea>
-
-            <v-select
-                v-model="app.users"
-                :items="states"
-                label="Select Users"
-                multiple
-                chips
-                hint="Select the users to access this application"
-                persistent-hint
-                ></v-select>
-
-            
-            
-            <v-switch
-              v-model="app.public"
-              color="success"
-              :label="`Public`"
-            ></v-switch>
-
-            <v-switch
-              v-model="app.security.active"
-              color="red"
-              :label="`Security`"
-            ></v-switch>
-
+            <v-text-field v-model="app.name" :counter="10" label="Name" required outlined dense hide-details class="mb-3" ></v-text-field>
+            <v-textarea v-model="app.descrip" name="input-7-1" label="Description" value="" outlined dense hide-details class="mb-3" ></v-textarea>
+            <v-select v-model="app.users" :items="states" label="Select Users" multiple chips outlined dense class="mb-3" hint="Select the users to access this application" persistent-hint ></v-select>
+            <v-switch v-model="app.public" color="success" :label="`Public`" ></v-switch>
+            <v-switch v-model="app.security.active" color="red" :label="`Security`" ></v-switch>
             <template v-if="app.security.active == true">
-              <p class="">
+              
+              
+              <!-- <p class="">
                 <strong>Token:</strong> <br />
                 {{ app.security.token }}
                 
-              </p>
-              <p class="">
+              </p> -->
+              <v-text-field 
+              v-model="app.security.token" 
+              label="Token" outlined dense 
+              inverted
+              class="mb-3 mt-3" readonly
+              append-icon="refresh"
+              @click:append="refreshToken"
+              ></v-text-field>
+              <!-- <p class="">
                 <v-btn
                     @click="refreshToken"
                     text
@@ -70,7 +38,7 @@
                   >
                     Refresh
                 </v-btn>
-              </p>
+              </p> -->
             </template>
 
              <p class="text-center">
@@ -78,7 +46,7 @@
                    <!-- <v-btn class="ma-2" tile outlined color="success" @click.prevent="createApp">
                         Create and Save Application
                     </v-btn> -->
-                    <v-btn depressed @click.prevent="createApp">Create Application</v-btn>
+                    <v-btn dark color="deep-purple darken-1" depressed @click.prevent="createApp">Create Application</v-btn>
 
                </p>
 
